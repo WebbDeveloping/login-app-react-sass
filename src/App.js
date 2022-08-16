bde21b97ddf044bbec3fc6baff513aa6093768c2
Joe Webb <joewebb@Joes-MacBook-Pro.local>
2022-08-16 14:11:22 -0600

    added sass and js files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import EmailConfirmation from './components/EmailConfirmation';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-Welcome to login app
+      <Login />
     </div>
   );
 }
